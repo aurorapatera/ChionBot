e1b4cb1023f13409937ea5cfd39286e9da82cbfb
aurorapatera <aurora.patera@gmail.com>
2022-01-13 16:51:49 +0100

    Smutword DB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
+const smutWord = require('/smutwords.json');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
@@ -9,10 +10,7 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-const smutWord = ["buttplug", "dildo", "titties", "titty", "pit", "porn", "dong",
-    "hand holding", "peen", "boob", "dick", "salami", "wizard tower",
-    "Biggus Dickus", "bite me", "eggplant", "peepee", "sword of the frontier",
-    "vlaakith", "shar preserve me", "horny", "Haedir"]
+
 client.on('messageCreate', (message) => {
   if (message.author.bot) return false;
 
