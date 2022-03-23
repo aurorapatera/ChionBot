332e8bbcc4e0b9fb9e4d4a2628b8b7d7297748a4
aurorapatera <aurora.patera@gmail.com>
2022-03-23 23:17:16 +0100

    added QOTD test

---
+++ deploy-commands.js
@@ -0,0 +1,6 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId } = process.env.clientId ; 
+const { guildId } = process.env.guildId ;
+const { token } = process.env.token ;
